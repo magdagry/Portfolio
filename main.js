bbf47a01780467b75ca2da38fef6a31babf21bc7
Lenka <magdagry16@gmail.com>
2023-07-06 20:22:55 +0200

    About Me section

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ var typed = new Typed('.typer', {
     backspeed: 100,
     loop:true
   });
+
+
+  let valueNumbers=document.querySelectorAll(".exp-number");
+  let interval=1000;
+
+  valueNumbers.forEach((valueNumber)=> {
+    let startValue=0;
+    let endValue=parseInt(valueNumber.getAttribute("data-val"));
+    let duration=Math.floor(interval/endValue);
+    let counter=setInterval(()=>{
+        startValue+=1;
+        valueNumber.textContent=startValue;
+        if(startValue==endValue) {
+            clearInterval(counter);
+        }
+    },duration)
+  })
